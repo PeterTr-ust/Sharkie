9b5832b6bf88f3932e5e69f052d48bf924cf910b
Peter Trözmüller <peterdaniel.tr@outlook.com>
2025-06-18 17:21:56 +0200

    added JSDOC for status-bar.class.js

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,3 +1,7 @@
+/**
+ * Represents a generic status bar (e.g., for health, coins, poison).
+ * Inherits visual rendering functionality from DrawableObject.
+ */
 class StatusBar extends DrawableObject {
     percentage = 100;
 
@@ -5,12 +9,20 @@ class StatusBar extends DrawableObject {
         super();
     }
 
+    /**
+     * Sets the current percentage and updates the image accordingly.
+     * @param {number} percentage - A number between 0 and 100.
+     */
     setPercentage(percentage) {
         this.percentage = percentage;
         let path = this.IMAGES[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
 
+    /**
+     * Resolves which image index to use based on the current percentage.
+     * @returns {number} Index corresponding to the appropriate image.
+     */
     resolveImageIndex() {
         if (this.percentage === 100) {
             return 5;
