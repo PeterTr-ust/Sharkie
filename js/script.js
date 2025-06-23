cef69d1293b7dabe869daabe19c4e5dcd680b591
Peter Trözmüller <peterdaniel.tr@outlook.com>
2025-06-23 21:45:30 +0200

    started with fullscreen logic

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function toggleFullscreen() {
+    const canvas = document.getElementById('canvas');
+    const icon = document.getElementById('fullscreen-icon');
+
+    if (!document.fullscreenElement) {
+        // Fullscreen aktivieren für das Canvas
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen();
+        } else if (canvas.msRequestFullscreen) {
+            canvas.msRequestFullscreen();
+        }
+    } else {
+        // Fullscreen verlassen
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
+
