29e1fd432771df460e72cc7c314289cd6a063fcd
Peter Trözmüller <peterdaniel.tr@outlook.com>
2025-06-23 15:17:24 +0200

    implemented inactivity animation for character

--- models/character.class.js
+++ models/character.class.js
@@ -28,6 +28,22 @@ class Character extends MovableObject {
         'img/character/idle/17.png',
         'img/character/idle/18.png'
     ];
+    IMAGES_INACTIVE = [
+        'img/character/inactive/sharkie-inactive-1.png',
+        'img/character/inactive/sharkie-inactive-2.png',
+        'img/character/inactive/sharkie-inactive-3.png',
+        'img/character/inactive/sharkie-inactive-4.png',
+        'img/character/inactive/sharkie-inactive-5.png',
+        'img/character/inactive/sharkie-inactive-6.png',
+        'img/character/inactive/sharkie-inactive-7.png',
+        'img/character/inactive/sharkie-inactive-8.png',
+        'img/character/inactive/sharkie-inactive-9.png',
+        'img/character/inactive/sharkie-inactive-10.png',
+        'img/character/inactive/sharkie-inactive-11.png',
+        'img/character/inactive/sharkie-inactive-12.png',
+        'img/character/inactive/sharkie-inactive-13.png',
+        'img/character/inactive/sharkie-inactive-14.png',
+    ];
     IMAGES_SWIM = [
         'img/character/swim/character-swim-1.png',
         'img/character/swim/character-swim-2.png',
@@ -75,6 +91,7 @@ class Character extends MovableObject {
     constructor(soundManager) {
         super().loadImg('img/character/idle/1.png');
         this.loadImgs(this.IMAGES_IDLE);
+        this.loadImgs(this.IMAGES_INACTIVE);
         this.loadImgs(this.IMAGES_SWIM);
         this.loadImgs(this.IMAGES_DEAD);
         this.loadImgs(this.IMAGES_HURT_BY_PUFFERFISH);
@@ -84,33 +101,56 @@ class Character extends MovableObject {
     }
 
     /**
-    * Handles character movement and animation updates.
+    * Sets up a global inactivity timer that resets on any key press.
+    *
+    * @returns {{ getInactivityDuration: function(): number, reset: function(): void }}
     */
+    setupInactivityTimer() {
+        let lastInputTime = Date.now();
+
+        const resetInactivityTimer = () => {
+            lastInputTime = Date.now();
+        };
+
+        document.addEventListener('keydown', resetInactivityTimer);
+
+        return {
+            getInactivityDuration: () => Date.now() - lastInputTime,
+            reset: resetInactivityTimer
+        };
+    }
+
+    /**
+   * Handles character movement and animation updates.
+   */
     animate() {
+        const { getInactivityDuration, reset } = this.setupInactivityTimer();
+
         setInterval(() => {
-            if (this.world && this.world.keyboard) {
-                if (this.world.keyboard.RIGHT && this.positionX < this.world.level.levelEndX) {
+            const kb = this.world?.keyboard;
+            if (kb) {
+                if (kb.RIGHT && this.positionX < this.world.level.levelEndX) {
                     this.moveRight();
                     this.otherDirection = false;
                     this.soundManager.play('swim');
+                    reset();
                 }
-
-                if (this.world.keyboard.LEFT && this.positionX > 0) {
+                if (kb.LEFT && this.positionX > 0) {
                     this.moveLeft();
                     this.otherDirection = true;
                     this.soundManager.play('swim');
+                    reset();
                 }
-
-                if ((this.world.keyboard.UP) && this.isOnTop()) {
+                if (kb.UP && this.isOnTop()) {
                     this.moveUp();
                     this.soundManager.play('swim');
+                    reset();
                 }
-
-                if ((this.world.keyboard.DOWN) && this.isOnBottom()) {
+                if (kb.DOWN && this.isOnBottom()) {
                     this.moveDown();
                     this.soundManager.play('swim');
+                    reset();
                 }
-
                 this.world.cameraX = -this.positionX;
             }
         }, 1000 / 60);
@@ -119,12 +159,10 @@ class Character extends MovableObject {
             if (this.isDead()) {
                 this.playAnimation(this.IMAGES_DEAD);
             } else if (this.isHurt()) {
+                reset(); // auch hier
                 const enemy = this.lastHitByEnemy;
-
                 if (enemy instanceof PufferFish) {
                     this.playAnimation(this.IMAGES_HURT_BY_PUFFERFISH);
-                } else if (enemy instanceof JellyFish) {
-                    this.playAnimation(this.IMAGES_HURT_BY_JELLYFISH);
                 } else {
                     this.playAnimation(this.IMAGES_HURT_BY_JELLYFISH);
                 }
@@ -135,10 +173,11 @@ class Character extends MovableObject {
                 this.world.keyboard.DOWN
             ) {
                 this.playAnimation(this.IMAGES_SWIM);
+            } else if (getInactivityDuration() > 15000) {
+                this.playAnimation(this.IMAGES_INACTIVE);
             } else {
                 this.playAnimation(this.IMAGES_IDLE);
             }
         }, 150);
-
     }
 }
